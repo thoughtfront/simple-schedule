ea87d099cf0857ee8c878ef82cfb9696a7a0539f
Christopher Dimoulis <christopherjamesd@gmail.com>
2019-01-19 16:49:40 -0600

    Override clearance passwords controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :passwords, controller: 'clearance/passwords', only: [:create, :new]
   resource :session, controller: 'clearance/sessions', only: [:create, :destroy]
   resources :users, controller: 'clearance/users', only: Clearance.configuration.user_actions do
-    resource :password, controller: 'clearance/passwords', only: [:create, :edit, :update]
+    resource :password, controller: 'clearance/passwords', only: [:create, :update]
   end
 
 end
