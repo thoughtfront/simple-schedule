8710f889872db03763e934679b414c35059a5709
Ricky <richard.l.laskowski@gmail.com>
2019-01-21 20:22:54 -0600

    Added location model test for requiring a name

--- spec/models/core/location_spec.rb
+++ spec/models/core/location_spec.rb
@@ -10,4 +10,11 @@ RSpec.describe Core::Location, type: :model do
     expect(test_location.save).to be_truthy
     end
   end
+
+  context 'validations' do
+    it 'requires a name' do
+      test_location = build(:core_location, name: nil)
+      expect(test_location.valid?).to be_falsey
+    end
+  end
 end
