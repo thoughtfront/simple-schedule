b09033f19b32035dc276271ca050a172e04c43d9
Christopher Dimoulis <christopherjamesd@gmail.com>
2019-01-21 22:27:05 -0600

    Add options route for session

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :passwords, controller: 'clearance/passwords', only: [:create, :new]
-  resource :session, controller: 'clearance/sessions', only: [:create, :destroy]
-  resources :users, controller: 'clearance/users', only: Clearance.configuration.user_actions do
-    resource :password, controller: 'clearance/passwords', only: [:create, :update]
+  resources :passwords, controller: 'auth/passwords', only: [:create, :new]
+  resource :session, controller: 'auth/sessions', only: [:create, :destroy] do
+    match '', via: :options, action: 'options'
+  end
+  resources :users, controller: 'auth/users', only: Clearance.configuration.user_actions do
+    resource :password, controller: 'auth/passwords', only: [:create, :update]
   end
 
   get '/test' => 'test#index', as: 'test_index'
