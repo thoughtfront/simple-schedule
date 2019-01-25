f113cb3b8caf0f8cb8bffc00912975c4f9954d1e
Ricky <richard.l.laskowski@gmail.com>
2019-01-25 13:11:22 -0600

    Added Event Category spec and factory

---
+++ spec/factories/core_event_categories.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+    factory :core_event_category, class: 'Core::EventCategory' do
+        
+        name {'test-name'}
+        description {'test description'}
+
+    end
+end
