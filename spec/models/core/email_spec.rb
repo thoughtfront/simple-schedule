855320d30cab19b6274f56320b23c2b10d1f5044
Ricky <richard.l.laskowski@gmail.com>
2019-01-26 17:00:23 -0600

    Added Email Spec

---
+++ spec/models/core/email_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Core::Email, type: :model do
+
+  context 'associations' do
+    it {is_expected.to have_one(:user)}
+    # it {is_expected.to have_one(:organization)}
+  end
+
+  context 'validations' do
+    it 'requires a value' do
+      test_email = build(:core_email, name:nil)
+      expect(test_email.valid?).to be(false)
+    end
+  end
+  
+end
