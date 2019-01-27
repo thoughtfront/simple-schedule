1efc3c215817d297a8b61e04ba4795b7c8661521
Christopher Dimoulis <christopherjamesd@gmail.com>
2019-01-26 19:37:53 -0600

    hotfix for missing uuid in emails table

--- db/migrate/20190126141332_create_core_emails.rb
+++ db/migrate/20190126141332_create_core_emails.rb
@@ -1,9 +1,9 @@
 class CreateCoreEmails < ActiveRecord::Migration[5.2]
   def change
-    create_table :core_emails do |t|
+    create_table :core_emails, id: :uuid, default: 'uuid_generate_v4()', force: true do |t|
       t.string :value, null: false
       t.string :label
-      
+
       t.timestamps
     end
   end
