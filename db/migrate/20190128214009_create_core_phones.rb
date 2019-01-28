0f69399c1d063128439d170fa93ac1d24d0e40f0
Ricky <richard.l.laskowski@gmail.com>
2019-01-28 16:47:19 -0600

    Added Phone Migration

--- db/migrate/20190128214009_create_core_phones.rb
+++ db/migrate/20190128214009_create_core_phones.rb
@@ -1,6 +1,8 @@
 class CreateCorePhones < ActiveRecord::Migration[5.2]
   def change
-    create_table :core_phones do |t|
+    create_table :core_addresses, id: :uuid, default: 'uuid_generate_v4()', force: true do |t|  
+      t.value
+      t.label
 
       t.timestamps
     end
