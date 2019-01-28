f34dbffbc88b340d11cfccb9669cba53cbc23ce8
Christopher Dimoulis <christopherjamesd@gmail.com>
2019-01-28 10:21:26 -0600

    Annotate contacts

--- spec/factories/core_contacts.rb
+++ spec/factories/core_contacts.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: core_contacts
+#
+#  id                 :uuid             not null, primary key
+#  prefix             :string
+#  first_name         :string           not null
+#  middle_name        :string
+#  last_name          :string           not null
+#  suffix             :string
+#  gender             :string
+#  birth_date         :date
+#  organization_id    :uuid             not null
+#  primary_email_id   :uuid             not null
+#  primary_phone_id   :uuid
+#  primary_address_id :uuid
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+
 FactoryBot.define do
     factory :core_contact, class: 'Core::Contact' do
 
