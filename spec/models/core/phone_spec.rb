e41a0002fa2d4f203505d31f445fbf3bd542a809
Ricky <richard.l.laskowski@gmail.com>
2019-01-30 11:49:02 -0600

    Fixed formatting of phone_spec.rb file

--- spec/models/core/phone_spec.rb
+++ spec/models/core/phone_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe Core::Phone, type: :model do
   context 'associations' do
     # it {is_expected.to have_one(:organization)}
     # it {is_expected.to have_many(:contacts)}
+  end
 
-    context 'validations' do
-      it 'requires a value' do
-        test_phone = build(:core_phone, value: nil)
-        expect(test_phone.valid?).to be(false)
-      end
+  context 'validations' do
+    it 'requires a value' do
+      test_phone = build(:core_phone, value: nil)
+      expect(test_phone.valid?).to be(false)
     end
   end
 end
