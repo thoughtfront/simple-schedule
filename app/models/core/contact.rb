3a7e3aa6894aa5e7680a6df95071b480dab915b9
Ricky <richard.l.laskowski@gmail.com>
2019-02-04 20:55:57 -0600

    Added many many relationship between contact and phone

--- app/models/core/contact.rb
+++ app/models/core/contact.rb
@@ -23,7 +23,7 @@ class Core::Contact < ApplicationRecord
   #Associations
   has_one :user
   # have_many :addresses
-  # have_many :phones
+  has_and_belongs_to_many :phones
   # have_many :emails
   # belong_to :organization
   # belong_to :primary_email
