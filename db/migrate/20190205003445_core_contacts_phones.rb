dcd0cca10ff4a0a393bf3884be0c36ca2b067a39
Christopher Dimoulis <christopherjamesd@gmail.com>
2019-02-09 16:56:44 -0600

    Remove timestamps from contacts phones join table

--- db/migrate/20190205003445_core_contacts_phones.rb
+++ db/migrate/20190205003445_core_contacts_phones.rb
@@ -1,10 +1,8 @@
 class CoreContactsPhones < ActiveRecord::Migration[5.2]
   def change
-    create_table :core_contacts_phones, id: :uuid, default: 'uuid_generate_v4()', force: true do |t|  
+    create_table :core_contacts_phones, id: :uuid, default: 'uuid_generate_v4()', force: true do |t|
       t.string :phone_id
       t.string :contact_id
-
-      t.timestamps
     end
   end
 end
