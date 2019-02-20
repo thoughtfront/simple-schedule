6744936fa3a669019be2aeb0cddab738dc434824
Christopher Dimoulis <christopherjamesd@gmail.com>
2019-02-20 08:31:37 -0600

    Fixes to get sign_in working

--- spec/factories/clearance.rb
+++ spec/factories/clearance.rb
@@ -1,10 +1,10 @@
-FactoryBot.define do
-  sequence :email do |n|
-    "user#{n}@example.com"
-  end
-
-  factory :user do
-    email {:email}
-    password {"password"}
-  end
-end
+# FactoryBot.define do
+#   sequence :email do |n|
+#     "user#{n}@example.com"
+#   end
+#
+#   factory :user do
+#     email {:email}
+#     password {"password"}
+#   end
+# end
