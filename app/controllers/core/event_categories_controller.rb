67e739088f3f7f367bd62d656757a3b129e136fd
Ricky <richard.l.laskowski@gmail.com>
2019-03-08 12:24:27 -0600

    Deleted data key

--- app/controllers/core/event_categories_controller.rb
+++ app/controllers/core/event_categories_controller.rb
@@ -4,75 +4,37 @@ class Core::EventCategoriesController < ApplicationController
     def create
         category = Core::EventCategory.new(event_category_params)
         if category.save
-
-            response.headers["Category Name: "] = "#{category.name}"
-            response.headers["Category Description: "] = "#{category.description}"
-            response.headers["Category Created at: "] = "#{category.created_at}"
-            response.headers["Category Updated at: "] = "#{category.updated_at}"
-
-            render json: {data:category}, status: :ok
+            render json: category, status: :ok
         else
-            render json: {data:category.errors}, status: :unprocessable_entity
+            render json: category.errors, status: :unprocessable_entity
         end
     end
 
     def index
-        x = 1
         categories = Core::EventCategory.order('created_at DESC');
-       
-        categories.each do |category|
-            response.headers["Category #{x} Name: "] = "#{category.name}"
-            response.headers["Category #{x} Description: "] = "#{category.description}"
-            response.headers["Category #{x} Created at: "] = "#{category.created_at}"
-            response.headers["Category #{x} Updated at: "] = "#{category.updated_at}"
-            x += 1
-        end
-
-        # response.headers['Categories'] = categories[1].name
-        render json: {data:categories}, status: :ok
+        render json: categories, status: :ok
     end
 
     def show
-
         category = Core::EventCategory.find(params[:id])
-
-        response.headers["Category Name: "] = "#{category.name}"
-        response.headers["Category Description: "] = "#{category.description}"
-        response.headers["Category Created at: "] = "#{category.created_at}"
-        response.headers["Category Updated at: "] = "#{category.updated_at}"
-
-        render json: {data:category}, status: :ok
-
+        render json: category, status: :ok
     end
 
     def update
         category = Core::EventCategory.find(params[:id])
-
         if category.update_attributes(event_category_params)
-            
-            response.headers["Category Name: "] = "#{category.name}"
-            response.headers["Category Description: "] = "#{category.description}"
-            response.headers["Category Created at: "] = "#{category.created_at}"
-            response.headers["Category Updated at: "] = "#{category.updated_at}"
-
-            render json: {data:category}, status: :ok
-
+            render json: category, status: :ok
         else
-            render json: {status: 'ERROR', message: 'Event category not updated', data:category.errors}, status: :unprocessable_entity
+            render json: category.errors, status: :unprocessable_entity
         end
     end
 
     def destroy
         category = Core::EventCategory.find(params[:id])
         if category.destroy
-            response.headers["Category Name: "] = "#{category.name}"
-            response.headers["Category Description: "] = "#{category.description}"
-            response.headers["Category Created at: "] = "#{category.created_at}"
-            response.headers["Category Updated at: "] = "#{category.updated_at}"
-
-            render json: {data:category}, status: :ok
+            render json: category, status: :ok
         else
-            render json: {status: 'ERROR', message: 'Event category not destroyed', data:category.errors}, status: :unprocessable_entity
+            render json: category.errors, status: :unprocessable_entity
         end
     end
 
